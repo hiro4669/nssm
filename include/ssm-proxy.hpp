2943d16d6d848bf1f171567ee057e8743040ff2a
Hiroaki Fukuda <hiroaki@java2.jp>
2017-12-07 18:30:07 +0800

    add socket

--- include/ssm-proxy.hpp
+++ include/ssm-proxy.hpp
@@ -5,12 +5,42 @@
 
 void test();
 
+#define SERVER_PORT     8080            /* サーバ用PORT */
+#define SERVER_IP       0x00000000UL    /* サーバ用待ち受けIP */
+#define BUFFER_SIZE     1024            /* バッファバイト数 */
+
+/* クライアントからの接続を待つサーバの情報を表現する構造体 */
+typedef struct {
+    int                 wait_socket;    /* サーバ待ち受け用ソケット */
+    struct sockaddr_in  server_addr;    /* サーバ待ち受け用アドレス */
+} TCPSERVER_INFO;
+
+/* クライアントとの接続に関する情報を保存する構造体 */
+typedef struct {
+    int                 data_socket;    /* クライアントとの通信用ソケット */
+    struct sockaddr_in  client_addr;    /* クライアントのアドレス */
+} TCPCLIENT_INFO;
+
+
+
+
 class ProxyServer {
+private:
+	TCPSERVER_INFO server;
+	TCPCLIENT_INFO client;
+
+	bool open();
+	bool wait();
+
 public:
 	ProxyServer();
 	~ProxyServer();
 	bool init();
 	bool run();
+	bool server_close();
+	bool client_close();
+	void handleCommand();
+
 };
 
 #endif
--- utilities/ssm-proxy.cpp
+++ utilities/ssm-proxy.cpp
@@ -1,4 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#include <errno.h>
 
 #include "ssm-proxy.hpp"
 
@@ -13,11 +22,91 @@ ProxyServer::~ProxyServer() {
 
 bool ProxyServer::init() {
 	printf("init\n");
+	memset(&this->server, 0, sizeof(this->server));
+	this->server.wait_socket = -1;
+	this->server.server_addr.sin_family      = AF_INET;
+	this->server.server_addr.sin_addr.s_addr = htonl(SERVER_IP);
+	this->server.server_addr.sin_port        = htons(SERVER_PORT);
+
+	return this->open();
+}
+
+bool ProxyServer::open() {
+	this->server.wait_socket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (this->server.wait_socket == -1) {
+		perror("open socket error");
+		return false;
+	}
+	if (bind(this->server.wait_socket, (struct sockaddr*)&this->server.server_addr,
+			sizeof(this->server.server_addr)) == -1) {
+		perror("server bind");
+		return false;
+	}
+	if (listen(this->server.wait_socket, 5) == -1) {
+		perror("server open");
+		return false;
+	}
+
 	return true;
 }
 
+bool ProxyServer::wait() {
+	memset(&this->client, 0, sizeof(this->client));
+	this->client.data_socket = -1;
+	for (;;) {
+		socklen_t client_addr_len = sizeof(this->client.client_addr);
+		this->client.data_socket = accept(this->server.wait_socket,
+				(struct sockaddr*)&this->client.client_addr,
+				&client_addr_len);
+
+		if (this->client.data_socket != -1) break;
+		if (errno == EINTR) continue;
+		perror("server open accept");
+		return false;
+	}
+	return true;
+}
+
+bool ProxyServer::server_close() {
+	if (this->server.wait_socket != -1) {
+		close(this->server.wait_socket);
+		this->server.wait_socket = -1;
+	}
+	return true;
+}
+
+bool ProxyServer::client_close() {
+	if (this->client.data_socket != -1) {
+		close(this->client.data_socket);
+		this->client.data_socket = -1;
+	}
+	return true;
+}
+
+void ProxyServer::handleCommand() {
+	printf("handlecommand\n");
+}
+
+
+
+
 bool ProxyServer::run() {
 	printf("run\n");
+	while(wait()) {
+		pid_t child_pid = fork();
+		if (child_pid == -1) { // fork failed
+			break;
+		} else if (child_pid == 0) { // child
+			this->server_close();
+			this->handleCommand();
+			this->client_close();
+			printf("end of process");
+			exit(1);
+		} else { // parent
+			this->client_close();
+		}
+	}
+	server_close();
 	return true;
 }
 
@@ -26,7 +115,8 @@ bool ProxyServer::run() {
 
 int main(void) {
 	ProxyServer server;
-	printf("Hello Proxy\n");
+	server.init();
+	server.run();
 	test();
 	
 	return 0;
